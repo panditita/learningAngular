cbd8ec455e24503021eda591417a3e98a4c86452
Etza <topimorita@gmail.com>
2019-09-27 15:13:39 +0100

    practicing angular HTTP Client

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -7,11 +7,18 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 const appRoutes: Routes = [ { path: ' ', component: HomeComponent }, { path: 'list', component: ListComponent } ];
 @NgModule({
 	declarations: [ AppComponent, HomeComponent, ListComponent ],
-	imports: [ BrowserModule, AppRoutingModule, FormsModule, RouterModule.forRoot(appRoutes, { enableTracing: true }) ],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		RouterModule.forRoot(appRoutes, { enableTracing: true }),
+		HttpClientModule
+	],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
