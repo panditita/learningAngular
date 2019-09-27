f6203cd7f35cb1039f963affc24f18e6d8753d93
Etza <topimorita@gmail.com>
2019-09-27 13:48:16 +0100

    practicing services and routing

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,9 +8,10 @@ import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 import { FormsModule } from '@angular/forms';
 
+const appRoutes: Routes = [ { path: ' ', component: HomeComponent }, { path: 'list', component: ListComponent } ];
 @NgModule({
 	declarations: [ AppComponent, HomeComponent, ListComponent ],
-	imports: [ BrowserModule, AppRoutingModule, FormsModule ],
+	imports: [ BrowserModule, AppRoutingModule, FormsModule, RouterModule.forRoot(appRoutes, { enableTracing: true }) ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
--- myapp/src/app/list/list.component.ts
+++ myapp/src/app/list/list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+	selector: 'app-list',
+	templateUrl: './list.component.html',
+	styleUrls: [ './list.component.scss' ]
 })
 export class ListComponent implements OnInit {
+	constructor(private _http: HttpService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this._http.myMethod();
+	}
 }
